db38489a67a8ff308d22a4621c4ee77b08040ce3
rodubeda-vyg <rubeda@valorygestion.cl>
2024-04-24 20:01:52 -0400

    init-structure

---
+++ Interfaces/Interface.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_intranet_net8.Interfaces
+{
+    public interface Interface
+    {
+        
+    }
+}
---
+++ Services/Service.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_intranet_net8.Services
+{
+    public class Service
+    {
+        
+    }
+}
